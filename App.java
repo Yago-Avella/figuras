33c253a76bc0d250d0cc1fbe8f560faf0255f66c
Yago Serer <yagovbox@gmail.com>
2024-12-02 20:56:08 +0100

    Actualizacion del fichero segunda vez App.java

--- App.java
+++ App.java
@@ -7,7 +7,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int option;
         do {
-            System.out.println("Calculadora de Area by Yago Serer Avella");
+            System.out.println("Calculadora de Area de Yago Serer");
 	    System.out.println("Selecciona una opción:");
             System.out.println("1. Círculo");
             System.out.println("2. Cuadrado");
